038fa3e0dc7f56108565cd7f3f93134b5a71ff77
glact1973 <44960587+glact1973@users.noreply.github.com>
2018-12-08 04:33:51 +0900

    スコア表示機能を追加
    スコア計算、スコア表示はTypingTutorViewの別関数にしたかったのですが、thisの意味が理解できていないのと、関数を作り引数にinput、targetを入れても、なぜか値が引き継がれず、時間もないので、handleKeyStrokeに追記しました。

--- public/js/components/TypingTutorGame.js
+++ public/js/components/TypingTutorGame.js
@@ -6,6 +6,11 @@ class TypingTutorGame {
     this.isRoundInProgress = false;
     this.currentStrokeCount = -1;
     this.targetText = null;
+    //入力文字を格納する
+    this.inputText = [];
+    this.accurateInputNum = 0;
+    this.totalScore = 0;
+    //追加ここまで
   }
 
   init() {
@@ -22,10 +27,27 @@ class TypingTutorGame {
   }
 
   handleKeyStroke(key) {
-    if (!this.isRoundInProgress) return;
-    this.currentStrokeCount += 1;
-    const targetChar = this.targetText[this.currentStrokeCount];
+    //テスト用の例文として固有値を設定するための固定値
+    //this.targetText = ['a','b','c']
+    if (!this.isRoundInProgress) return; 
+    this.currentStrokeCount += 1; 
+    const targetChar = this.targetText[this.currentStrokeCount]; 
     this.view.renderKeystroke(key, targetChar);
+    //追記ここから
+    //例文と同様に、入力文字を配列に格納する
+    this.inputText.push(key);
+    //入力済文字数のindex(currentStrokeCount)が例文(targetText)の文字数と等しい場合、スコア表示を行う
+    if (this.currentStrokeCount === this.targetText.length-1 ) {
+      //1文字ずつ例文と一致しているか判定し、一致していれば一致した文字数を1つ足す
+      for (let i = 0; i <= this.currentStrokeCount; i ++) {
+        if (this.inputText[i] === this.targetText[i]) this.accurateInputNum ++
+      }
+      //スコアを計算する
+      this.totalScore = Math.floor((this.accurateInputNum / (this.currentStrokeCount+1))*100);
+      //スコアを表示する
+      console.log('SCORE: '+ this.totalScore)
+    }
+    //追記ここまで
   }
 
   initTargetText() {
